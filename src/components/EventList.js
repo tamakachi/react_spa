e8d10f85470bb4edbc82062b1f9ff59c2773d39a
Kent <kenttechnical@gmail.com>
2022-09-12 14:32:39 +0200

    Component completed

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,5 +1,11 @@
-export default function EventList() {
-  return (
-    <></>
+export default function EventList({list, deleteClick}) {
+
+  
+    return (
+    <>{list.map((item)=>(
+        <div key={item.id}>
+          <h2>Name: {item.name}, Age: {item.age}</h2>
+          <button onClick={()=>{deleteClick(item.id)}}>Delete</button>
+        </div>))}</>
   )
 }
