9cc11ef628766e61ec235053d51d5d915503a47d
Kent <kenttechnical@gmail.com>
2022-09-13 13:26:29 +0200

    Inline styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,11 @@ import './Modal.css'
 export default function Modal(props) {
   return (
     <div className="modal-backdrop">
-      <div className="modal">
+      <div className="modal" style={{
+        border: "4px solid",
+        borderColor: "#ff4500",
+        tectAlign:"center"
+      }}>
         {props.children}
         <button onClick={props.hideModal}>Close</button>
       </div>
