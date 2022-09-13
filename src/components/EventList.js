983557aea2342e68fa2b4ad7895ae198288106b0
Kent <kenttechnical@gmail.com>
2022-09-13 14:35:05 +0200

    CSS Modules

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,9 +1,11 @@
+import styles from './EventList.module.css'
+
 export default function EventList({list, deleteClick}) {
 
   
     return (
     <>{list.map((item)=>(
-        <div key={item.id}>
+        <div className={styles.card} key={item.id}>
           <h2>Name: {item.name}, Age: {item.age}</h2>
           <button onClick={()=>{deleteClick(item.id)}}>Delete</button>
         </div>))}</>
