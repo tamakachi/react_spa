7629993806bdeb31cae1fa406d4caa95127120b6
Kent <kenttechnical@gmail.com>
2022-09-14 15:56:48 +0200

    Controlled inputs, binding values using value={}

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -10,14 +10,22 @@ export default function NewEventForm() {
     <form className="new-event-form">
         <label>
             <span>Event Title:</span>
-            <input type="text" onChange={(e)=>{setTitle(e.target.value)}}/>
+            <input 
+              type="text" 
+              onChange={(e)=>{setTitle(e.target.value)}}
+              value={title}/>
         </label>
         <label>
             <span>Event Date:</span>
-            <input type="date" onChange={(e)=>{setDate(e.target.value)}}/>
+            <input type="date" onChange={(e)=>{setDate(e.target.value)}} value={date}/>
         </label>
         <button>Submit</button>
         <p>Title: {title} Date: {date}</p>
+        <button 
+        onClick={(e)=>{
+          e.preventDefault()
+          setDate('') 
+          setTitle('')}}>Reset Values</button>
     </form>
   )
 }
